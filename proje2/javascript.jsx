de8cb344f07203aeb903ba3a2c3d057545e346a4
ozlem <ozlemakcicek83@gmail.com>
2023-10-25 16:09:19 +0200

    first commit

---
+++ proje2/javascript.jsx
@@ -0,0 +1,25 @@
+
+// 1 den 100 e kadar çift sayıları yazdıran döngüyü oluşturunuz.
+
+for(
+    let i=2;
+    i<=100;
+    i+=2
+    ){
+        console.log(i);
+    }
+
+
+
+// Ad ve soyadınızı birleştiren Javascript kodunu oluşturunuz.
+
+const ad='Özlem';
+const surname='Akcicek';
+
+console.log(`Benim adim ${ad} ${surname}.`);
+
+//* alternativ;
+
+const wer= ad + ' ' + surname;
+//or
+console.log('Benim adim' +' ' + wer);
